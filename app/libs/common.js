21ed36044a0a40ff18887c7f161e556559afce41
frontendercode <frontendercode@gmail.com>
2020-12-09 11:30:24 +0200

    v.0.1.6
    Тестировал валидацию формы с помощью jquery плагина

--- app/libs/common.js
+++ app/libs/common.js
@@ -1,13 +1,13 @@
 // $(document).ready(function(){
 $(document).ready(() => {
-  $(".header__menu-btn").on("click", () => {
-    $(".header__menu > ul").toggleClass("active");
+  $('.header__menu-btn').on('click', () => {
+    $('.header__menu > ul').toggleClass('active');
     $('.hamburger').toggleClass('is-active');
   });
 
-  $(".header__drop-down.drop-down a").on("click", () => {
-    $(".header__drop-down> a").toggleClass("arrows_top");
-    $(".drop-down__list").toggleClass("visible");
+  $('.header__drop-down.drop-down a').on('click', () => {
+    $('.header__drop-down> a').toggleClass('arrows_top');
+    $('.drop-down__list').toggleClass('visible');
   });
   // is - active;
   $('#fist__three').mask('+7(000)000-00-00', {
@@ -34,202 +34,108 @@ $(document).ready(() => {
     clearIfNotMatch: !0,
   });
 
-  $('#form-one').validate({
-    rules: {
-      myname: {
-        required: true,
-        minlength: 3,
-      },
-      myemail: {
-        required: true,
-        email: true,
-      },
-      myphone: {
-        required: true,
-        minlength: 16,
-      },
-    },
-    messages: {
-      myname: {
-        required: 'Введите нормальное имя',
-        minlength: jQuery.validator.format('Ваше имя не должно быть короче {0} символов!'),
-      },
-      myemail: {
-        required: 'Нам нужен ваш адрес электронной почты, чтобы связаться с вами',
-        email: 'Ваш адрес электронной почты должен быть в формате name@domain.com',
-      },
-      myphone: {
-        required: 'Нам нужен ваш номер телефона, чтобы связаться с вами',
-        minlength: jQuery.validator.format('Номер телефона не короче {0} символов!'),
-      },
-    },
-    validClass: 'help-inline filds-success',
-    errorClass: 'help-inline text-danger',
-    errorElement: 'span',
-    validElement: 'span',
-    highlight(element) {
-      $(element).parents('.form-group').addClass('has-error');
-    },
-    unhighlight(element) {
-      $(element).parents('.form-group').removeClass('has-error');
-      $(element).parents('.form-group').addClass('has-success');
-    },
-    submitHandler(form, e) {
-      e.preventDefault();
-      $.ajax({
-        type: 'POST',
-        url: 'mail.php',
-        dataType: 'html',
-        data: $('#form-one').serialize(),
-        success(result) {
-          $('.message').html('<span class="valide">Сообщение получено</span>');
-          console.info(result);
-          $('#form-one').trigger('reset').delay(2000);
-
-          setTimeout(() => {
-            $('#form-one .message').html('');
-            $('input.input_ferst').css('border', '3px solid transparent');
-          }, 2000);
-        },
-        error(error) {
-          console.info(error);
-          $('#form-one .message').html('<span class="novalide">Ошибка при отправке сообщения</span>');
-        },
-      });
-      return false;
-    },
-  });
-
-  $('#forming__two').validate({
-    rules: {
-      myname: {
-        required: true,
-        minlength: 3,
-      },
-      myemail: {
-        required: true,
-        email: true,
-      },
-      myphone: {
-        required: true,
-        minlength: 16,
-      },
-    },
-    messages: {
-      myname: {
-        required: 'Введите нормальное имя',
-        minlength: jQuery.validator.format('Ваше имя не должно быть короче {0} символов!'),
-      },
-      myemail: {
-        required: 'Нам нужен ваш адрес электронной почты, чтобы связаться с вами',
-        email: 'Ваш адрес электронной почты должен быть в формате name@domain.com',
-      },
-      myphone: {
-        required: 'Нам нужен ваш номер телефона, чтобы связаться с вами',
-        minlength: jQuery.validator.format('Номер телефона не короче {0} символов!'),
-      },
-    },
-    validClass: 'help-inline filds-success',
-    errorClass: 'help-inline text-danger',
-    errorElement: 'span',
-    validElement: 'span',
-    highlight(element) {
-      $(element).parents('.form-group').addClass('has-error');
-    },
-    unhighlight(element) {
-      $(element).parents('.form-group').removeClass('has-error');
-      $(element).parents('.form-group').addClass('has-success');
-    },
-    submitHandler(form, e) {
-      e.preventDefault();
-      $.ajax({
-        type: 'POST',
-        url: 'mail.php',
-        dataType: 'html',
-        data: $('#forming__two').serialize(),
-        success(result) {
-          $('#forming__two .message').html('<span class="valide">Сообщение получено</span>');
-          console.info(result);
-          $('#forming__two').trigger('reset').delay(2000);
-
-          setTimeout(() => {
-            $('#forming__two .message').html('');
-          }, 2000);
-        },
-        error(error) {
-          console.info(error);
-          $('#forming__two .message').html('<span class="novalide">Ошибка при отправке сообщения</span>');
-        },
-      });
-      return false;
-    },
-  });
-
-  // the end two
-  $('#forming__telgram').validate({
-    rules: {
-      myname: {
-        required: true,
-        minlength: 3,
-      },
-      myemail: {
-        required: true,
-        email: true,
-      },
-      myphone: {
-        required: true,
-        minlength: 16,
-      },
-    },
-    messages: {
-      myname: {
-        required: 'Введите нормальное имя',
-        minlength: jQuery.validator.format('Ваше имя не должно быть короче {0} символов!'),
-      },
-      myemail: {
-        required: 'Нам нужен ваш адрес электронной почты, чтобы связаться с вами',
-        email: 'Ваш адрес электронной почты должен быть в формате name@domain.com',
-      },
-      myphone: {
-        required: 'Нам нужен ваш номер телефона, чтобы связаться с вами',
-        minlength: jQuery.validator.format('Номер телефона не короче {0} символов!'),
-      },
-    },
-    validClass: 'help-inline filds-success',
-    errorClass: 'help-inline text-danger',
-    errorElement: 'span',
-    validElement: 'span',
-    highlight(element) {
-      $(element).parents('.form-group').addClass('has-error');
-    },
-    unhighlight(element) {
-      $(element).parents('.form-group').removeClass('has-error');
-      $(element).parents('.form-group').addClass('has-success');
-    },
-    submitHandler(form, e) {
-      e.preventDefault();
-      $.ajax({
-        type: 'POST',
-        url: 'telegram-mail.php',
-        dataType: 'html',
-        data: $('#forming__telgram').serialize(),
-        success(result) {
-          $('#forming__telgram .message').html('<span class="valide">Сообщение получено</span>');
-          console.info(result);
-          $('#forming__telgram').trigger('reset').delay(2000);
-
-          setTimeout(() => {
-            $('#forming__telgram .message').html('');
-          }, 2000);
-        },
-        error(error) {
-          console.info(error);
-          $('#forming__telgram .message').html('<span class="novalide">Ошибка при отправке сообщения</span>');
-        },
-      });
-      return false;
-    },
-  });
-
   // the end three
+});
+
+$(document).ready(() => {
+  $('[data-submit]').on('click', function (e) {
+    e.preventDefault();
+    $(this).parent('form').submit();
+  });
+  $.validator.addMethod(
+    'regex',
+    function (value, element, regexp) {
+      const re = new RegExp(regexp);
+      return this.optional(element) || re.test(value);
+    },
+    'Please check your input.',
+  );
+
+  // Функция валидации и вывода сообщений
+  function valEl(el) {
+    el.validate({
+      rules: {
+        // tel: {
+        //     required: true,
+        //     regex: '^([\+]+)*[0-9\x20\x28\x29\-]{5,20}$'
+        // },
+        name: {
+          required: true,
+        },
+        email: {
+          required: true,
+          email: true,
+        },
+      },
+      messages: {
+        // tel: {
+        //     required: 'Поле обязательно для заполнения',
+        //     regex: 'Телефон может содержать символы + - ()'
+        // },
+        name: {
+          required: 'Поле обязательно для заполнения',
+        },
+        email: {
+          required: 'Поле обязательно для заполнения',
+          email: 'Неверный формат E-mail',
+        },
+      },
+
+      // Начинаем проверку id="" формы
+      submitHandler(form) {
+        $('#loader').fadeIn();
+        const $form = $(form);
+        const $formId = $(form).attr('id');
+        switch ($formId) {
+        // Если у формы id="goToNewPage" - делаем:
+        case 'goToNewPage':
+          $.ajax({
+            type: 'POST',
+            url: 'mail.php',
+            // url: $form.attr('action'),
+            data: $form.serialize(),
+          }).always((response) => {
+            // ссылка на страницу "спасибо" - редирект
+            // location.href = 'https://';
+            document.getElementById('formPreview').innerHTML = 'Письмо отправлено ';
+            // отправка целей в Я.Метрику и Google Analytics
+            ga('send', 'event', 'masterklass7', 'register');
+            yaCounter27714603.reachGoal('lm17lead');
+          });
+          break;
+          // Если у формы id="popupResult" - делаем:
+        case 'popupResult':
+          $.ajax({
+            type: 'POST',
+            url: 'mail.php',
+            //     url: $form.attr('action'),
+            data: $form.serialize(),
+          }).always((response) => {
+            setTimeout(() => {
+              $('#loader').fadeOut();
+            }, 800);
+            setTimeout(() => {
+              $('#overlay').fadeIn();
+              $form.trigger('reset');
+              $('input, textarea').removeClass('error, valid');
+              // строки для остлеживания целей в Я.Метрике и Google Analytics
+            }, 1100);
+            $('#overlay').on('click', function (e) {
+              $(this).fadeOut();
+            });
+          });
+          if ($('*').is('#myModal')) {
+            $('#myModal').arcticmodal('close');
+          }
+          break;
+        }
+        return false;
+      },
+    });
+  }
+
+  // Запускаем механизм валидации форм, если у них есть класс .js-form
+  $('.form__basic').each(function () {
+    valEl($(this));
+  });
 });
